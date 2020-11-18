4e429f9f5859d64de290717625fe9df0d31b6487
amna1987 <74597210+amna1987@users.noreply.github.com>
2020-11-18 05:14:41 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<body bgcolor="grey">
+<?php
+$name="amna";
+$class="3it";
+$subject="php";
+$address="kh";
+$phone="0123875441";
+echo"my name is :".$name."<br>";
+echo"class:".$class."<br>";
+echo"subject:".$subject."<br>";
+echo"address:".$address."<br>";
+echo"phone:".$phone."<br>";
+?>
+</body>
+</html>
